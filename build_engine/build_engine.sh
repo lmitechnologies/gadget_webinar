66b3ee93ac1acc83a1e97d5cfba451c1b8c74ba6
Yijun Jiang <yijun1121@gmail.com>
2022-09-12 15:17:30 -0400

    up to date

--- build_engine/build_engine.sh
+++ build_engine/build_engine.sh
@@ -1,6 +1,10 @@
 # convert the weights file
-python3 /repos/yolov5/gen_wts.py -w /app/2022-07-15_512_512_600_s.pt -o /app/2022-07-15_512_512_600_s.wts
+if [ ! -f /app/2022-07-15_512_512_600_s.wts ]; then
+    python3 /repos/yolov5/gen_wts.py -w /app/2022-07-15_512_512_600_s.pt -o /app/2022-07-15_512_512_600_s.wts
+fi
 
 # generate engine
 mkdir -p /app/build && cd /app/build && cmake /repos/tensorrtx/yolov5 && make
-/app/build/yolov5 -c /app/2022-07-15.yaml -w /app/2022-07-15_512_512_600_s.wts -o /app/yolov5_arm_512x512_s.engine
+if [ ! -f /app/yolov5_arm_512x512_s.engine ]; then
+    /app/build/yolov5 -c /app/2022-07-15.yaml -w /app/2022-07-15_512_512_600_s.wts -o /app/yolov5_arm_512x512_s.engine
+fi
